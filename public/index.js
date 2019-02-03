6bee3db8e2ba3d4cbb67f0668350bfcde4c881db
unknown <kravchel16@gmail.com>
2019-02-03 14:52:38 +0200

    fixed click on button-buy

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ window.onload = function() {
     const itemsContainer = document.getElementById('items-container');
     const notBoughtItemsContainer = document.getElementById('not-bought-items-container');
     const boughtItemsContainer = document.getElementById('bought-items-container');
-    const notBoughtItems = [];
-    const boughtItems = [];
+    let notBoughtItems = [];
+    let boughtItems = [];
 
     $("#add-button").on( "click", function() {
         let productName = document.getElementById('input').value;
@@ -22,7 +22,7 @@ window.onload = function() {
             <button class="button-buy" data-product-name="${productName}" data-product-id="" data-product-amount="1">Куплено</button>
             <button class="delete-button" data-product-name="${productName}" data-product-id="">X</button>
         </div>
-    </div>`;
+        </div>`;
 
         notBoughtItems.push({name: productName, amount: 1});
         RenderNotBoughtItems();
@@ -33,11 +33,10 @@ window.onload = function() {
      
 
       $(document).on('click' , '.button-buy', function(){
-        // Еще нужно добалять в список купленых товаров и с кнопоками разобраться
         let _id = $(this).data('product-id');
         let _amount= $(this).closest('.item').find('.amount')[0].innerHTML;
         let _name = $(this).data('product-name');
-        $(this).closest('.item')[0].innerHTML=`<div class="item">
+        $(this).closest('.item')[0].innerHTML=`
         <div class="first-column titles">
             <span class="title crossed">${_name}</span>
         </div>
@@ -46,11 +45,38 @@ window.onload = function() {
         </div>
         <div class="third-column option-buttons">
             <button class="button-buy button-not-bought" data-product-name="${_name}" data-product-id="${_id}" data-product-amount="${_amount}">Не куплено</button>
-        </div>
-    </div>`;
+        </div>`;
         
+        notBoughtItems = notBoughtItems.filter(item => item.name!=_name);
+        RenderNotBoughtItems();
         boughtItems.push({id:_id, amount:_amount, name:_name});
         RenderBoughtItems();
+    });
+    
+    $(document).on('click' , '.button-not-bought', function(){
+        let _id = $(this).data('product-id');
+        // let _amount= $(this).closest('.item').find('.amount')[0].innerHTML;
+        let _amount = $(this).data('product-amount');
+        let _name = $(this).data('product-name');
+        console.log($(this).closest())
+        console.log($(this).closest('.item'));
+        $(this).parents('.item')[0].innerHTML= `
+        <div class="first-column titles">
+            <span class="title">${_name}</span>
+        </div>
+        <div class="second-column add-buttons">
+            <button class="circular-button red-button"><b>-</b></button>
+            <span class="amount">${_amount}</span>
+            <button class="circular-button green-button"><b>+</b></button>
+        </div>
+        <div class="third-column option-buttons">
+            <button class="button-buy" data-product-name="${_name}" data-product-id="${_id}" data-product-amount="${_amount}">Куплено</button>
+            <button class="delete-button" data-product-name="${_name}" data-product-id="${_id}">X</button>
+        </div>`;
+        
+        boughtItems = boughtItems.filter(item => item.name!=_name);
+        notBoughtItems.push({id:_id, amount:_amount, name:_name});
+        RenderNotBoughtItems();
     });
 
 
@@ -60,21 +86,21 @@ window.onload = function() {
               return (`<span class="product-item">
               ${item.name}
               <span class="circular-amount">${item.amount}</span>
-      </span>`);
+              </span>`);
           });
 
         notBoughtItemsContainer.innerHTML=displayNotBoughtItems.join(' ');  
       }
 
-      function RenderBoughtItems(){
+    function RenderBoughtItems(){
           
         let displayBoughtItems = boughtItems.map((item)=>{
               return (`<span class="product-item crossed">
               ${item.name}
               <span class="circular-amount crossed">${item.amount}</span>
-      </span>`);
+              </span>`);
           });
 
         boughtItemsContainer.innerHTML=displayBoughtItems.join(' ');  
-      }
+    }
 }
